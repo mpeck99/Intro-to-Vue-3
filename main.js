4a18c2421ec70e32ccd3a98b309be750d79fa38f
Morgan Peck <mlpeck@arkbluecross.com>
2023-07-25 10:00:37 -0500

    adding the content from following along with Intro to Vue 3

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-const product = 'Socks'
+const app = Vue.createApp({
+  data() {
+    return {
+      cart: [],
+      premium: true,
+      url: 'https://www.vuemastery.com/',
+    };
+  },
+  methods: {
+    updateCart(id) {
+      this.cart.push(id);
+    },
+  },
+});
